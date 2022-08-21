74d8e91561c473b2b8a6ec9a4aeeb1c3e28dc4a4
Oleksii <97940139+Tsakhlo@users.noreply.github.com>
2022-08-21 21:55:01 +0300

    Update fetchCountries.js

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -2,6 +2,9 @@ const BASE_URL = 'https://restcountries.com/v3.1'
 
 export function fetchCountries(name) {
   return fetch(`${BASE_URL}/name/${name}?fields=name,capital,population,flags,languages`)
-    .then(response => response.json())
-    .catch(error => console.log(error))
-}
+  .then(response => {      
+    if (!response.ok) {        
+        throw new Error('Что-то пошло не так', response.status);      
+    }      
+    return response.json();  
+})}
